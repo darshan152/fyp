d0d73840d1e1a8ae294f1c0b30b8121228c242a6
darshan152 <59141335+darshan152@users.noreply.github.com>
2022-10-04 10:14:46 +0800

    Add Delete functionality

--- fe/src/conponents/historycard.js
+++ fe/src/conponents/historycard.js
@@ -3,12 +3,16 @@ import { Card } from 'antd';
 import { useSelector } from 'react-redux'
 import { useDispatch } from 'react-redux'
 import { setPython, setRead } from '../states/cardModalSlice';
+import axios from 'axios';
 
 import './ribbon.css';
 import { setEditData } from '../states/editDataSlice';
+import { editStep } from '../states/stepsArrSlice';
+import { setCurrentData } from '../states/csvDataSlice';
 
 function HistoryCard(props) {
     const stepsArr = useSelector(state => state.stepsArr.value)
+    const originalData = useSelector(state => state.csvData.value.originalData)
     console.log(stepsArr)
     const dispatch = useDispatch()
 
@@ -28,10 +32,29 @@ function HistoryCard(props) {
 
     }
 
+    const handleDelete = (index) => {
+        let newSteps = [...stepsArr]
+        newSteps = newSteps.slice(0,index).concat(newSteps.slice(index+1,newSteps.length))
+        axios.post(`${process.env.REACT_APP_BACKEND_URL}/retransform`, {data:originalData,stepsArr:newSteps})
+        .then(res => {
+            dispatch(setCurrentData(res.data));
+            dispatch(editStep(newSteps));
+        })
+
+    }
+
     return(
         <div>
             {stepsArr.map(function(step, index){
-                    return <Card className='step' key={ index }>{step['type']}<button onClick={()=>handleEdit(step,index)}>Edit</button></Card>;
+                    return (
+                        <Card className='step' key={ index }>
+                            {step['type']}
+                            <button onClick={()=>handleEdit(step,index)}>Edit</button>
+                            { step['type'] !== 'read' ? 
+                            <button onClick={()=>handleDelete(index)}>Delete</button>
+                            : null}
+                        </Card>
+                    )
                   })}
         </div>
     );
